d9ce545dce3a10d3e8db0f15726a8ca2b8dd284e
Jose Gómez-Sellés <jldgso@gmail.com>
2021-02-14 14:23:04 +0100

    Fix devrun image version
    No version was set in devrun, maing the script to try to get latest.
    This commit fixes it by adding 0.1.0 tag.

--- devrun.sh
+++ devrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUILD_IMAGE_NAME="hermes_build"
+BUILD_IMAGE_NAME="hermes_build:0.1.0"
 COMPILED_EXECUTABLE="$1"
 shift
 docker run --rm -u "$(id -u):$(id -g)" -v "${PWD}":/code --entrypoint "${COMPILED_EXECUTABLE}" "${BUILD_IMAGE_NAME}" "$@"
