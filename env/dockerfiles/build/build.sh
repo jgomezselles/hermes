74c6369e9db409c9b34180053f15183526c55b4d
Jose Gómez-Sellés <jldgso@gmail.com>
2021-03-05 19:14:36 +0100

    Build hermes_prod from scratch
    This commit makes all dependencies explicit and removes
    hermes_build image. By doing so, scratch image as final base
    is enabled.

--- env/dockerfiles/build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Formatting phase
-find . -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\|c\|hh\)' -exec clang-format --verbose -style=file -i {} \;
-# Building phase
-mkdir -p build && cd build && cmake .. && make -j"$(nproc)"
