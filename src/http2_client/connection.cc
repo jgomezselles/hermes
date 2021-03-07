d10516210ca310452372bbf3b44c98417238083e
Jose Gómez-Sellés <jldgso@gmail.com>
2021-03-07 19:10:09 +0100

    Rework connection unit tests
    A unique_ptr was being used, when there is no need.
    Setup and TearDown of the connection is removed to be
    done always, because it causes a lot of pain in every test.
    
    A test case is removed because there is no guarantee of the
    on_error callback to be called when a connection cannot be
    established. For that reason a new function is also added
    to check for any status for a given time.
    
    This one will be used in the future everywhere, but a bigger
    refactor is needed.

--- src/http2_client/connection.cc
+++ src/http2_client/connection.cc
@@ -11,11 +11,11 @@ connection::connection(const std::string& h, const std::string& p)
     : io_service(),
       svc_work(boost::asio::io_service::work(io_service)),
       session(io_service, h, p),
-      status(status::NOT_OPEN)
+      connection_status(status::NOT_OPEN)
 {
     session.on_connect([this, h, p](tcp::resolver::iterator endpoint_it) {
         std::cerr << "Connected to " << h << ":" << p << std::endl;
-        status = status::OPEN;
+        connection_status = status::OPEN;
         status_change_cond_var.notify_one();
     });
 
@@ -37,34 +37,41 @@ connection::~connection()
         worker.join();
     }
 
-    if (status == status::OPEN)
+    if (connection_status == status::OPEN)
     {
         session.shutdown();
     }
 }
 
-void
-connection::notify_close()
+void connection::notify_close()
 {
-    status = status::CLOSED;
+    connection_status = status::CLOSED;
     status_change_cond_var.notify_one();
 }
 
-void
-connection::close()
+void connection::close()
 {
     session.shutdown();
     notify_close();
 }
 
-bool
-connection::wait_to_be_connected()
+bool connection::wait_to_be_connected()
 {
     std::unique_lock<std::mutex> lock(mtx);
     status_change_cond_var.wait_for(lock, std::chrono::duration<int, std::milli>(2000), [&] {
-        return (status != http2_client::connection::status::NOT_OPEN);
+        return (connection_status != status::NOT_OPEN);
     });
-    return (status == http2_client::connection::status::OPEN);
+    return (connection_status == status::OPEN);
+}
+
+bool connection::wait_for_status(const std::chrono::duration<int, std::milli>& max_time,
+                                 const  status& st)
+{
+    std::unique_lock<std::mutex> lock(mtx);
+    return status_change_cond_var.wait_for(lock, max_time, [this, &st]
+    {
+        return connection_status == st;
+    });
 }
 
 }  // namespace http2_client
