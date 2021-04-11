b84e24ed318bd660b8cf83d56b46d274e336d86a
Jose Gómez-Sellés <jldgso@gmail.com>
2021-04-11 19:27:49 +0200

    Add stats interface
    This class is created as an enabler for the classes which use
    stats to be tested, as it eases mock creation.

---
+++ src/stats/stats if.hpp
@@ -0,0 +1,20 @@
+
+#include <string>
+
+#pragma once
+
+namespace stats
+{
+
+class stats_if
+{
+public:
+    virtual ~stats_if() = default;
+
+    virtual void increase_sent(const std::string& id) = 0;
+    virtual void add_measurement(const std::string& id, const int64_t elapsed_time, const int code) = 0;
+    virtual void add_timeout(const std::string& id) = 0;
+    virtual void add_error(const std::string& id, const int e) = 0;
+    virtual void add_client_error(const std::string& id, const int e) = 0;
+};
+}  // namespace stats
