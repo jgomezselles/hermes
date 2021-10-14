89e48fd1dd70e94c07f99f8de5b133c6d6047933
Jose Gómez-Sellés <jldgso@gmail.com>
2021-10-14 14:57:45 +0200

    Remove newlines
    To keep with the rest of the files

--- src/script/script_reader.hpp
+++ src/script/script_reader.hpp
@@ -12,17 +12,11 @@ public:
     script_reader(const std::string &json);
 
     server_info build_server_info();
-
     int build_timeout();
-
     range_type build_ranges();
-
     std::deque<message> build_messages();
-
     message build_message(const std::string &m);
-
     msg_modifier build_message_modifier();
-
     std::map<std::string, std::string> build_variables();
 
 private:
