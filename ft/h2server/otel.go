278b4447076ee116ae8abe272883f563e494df3f
Jose Gomez-Selles <14234281+jgomezselles@users.noreply.github.com>
2026-05-23 10:52:09 +0200

    Bump up serverMock conflicting versions

--- ft/h2server/otel.go
+++ ft/h2server/otel.go
@@ -8,7 +8,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 
 	"go.opentelemetry.io/otel/log/global"
 	"go.opentelemetry.io/otel/propagation"
